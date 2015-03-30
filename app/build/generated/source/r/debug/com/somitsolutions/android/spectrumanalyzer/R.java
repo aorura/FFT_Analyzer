e254d520c668f43510ca710b189dfb81c1dd7589
aorura <hyponus@gmail.com>
2015-03-30 14:26:46 +0900

    add timeTask

--- app/build/generated/source/r/debug/com/somitsolutions/android/spectrumanalyzer/R.java
+++ app/build/generated/source/r/debug/com/somitsolutions/android/spectrumanalyzer/R.java
@@ -16,6 +16,7 @@ public final class R {
     public static final class id {
         public static final int button=0x7f050002;
         public static final int debugLabel=0x7f050004;
+        public static final int debugRedLabel=0x7f050005;
         public static final int editText=0x7f050001;
         public static final int mainLayOut=0x7f050000;
         public static final int webView=0x7f050003;
---
+++ app/src/main/java/com/somitsolutions/android/spectrumanalyzer/ObservableWebView.java
@@ -0,0 +1,53 @@
+package com.somitsolutions.android.spectrumanalyzer;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.webkit.WebView;
+
+/**
+ * Created by chalse.park on 2015-03-30.
+ */
+public class ObservableWebView extends WebView {
+    private OnScrollChangedCallback mOnScrollChangedCallback;
+
+    public ObservableWebView(final Context context)
+    {
+        super(context);
+    }
+
+    public ObservableWebView(final Context context, final AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public ObservableWebView(final Context context, final AttributeSet attrs, final int defStyle)
+    {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onScrollChanged(final int l, final int t, final int oldl, final int oldt)
+    {
+        super.onScrollChanged(l, t, oldl, oldt);
+        if(mOnScrollChangedCallback != null)
+            mOnScrollChangedCallback.onScroll(l, t, oldl, oldt);
+    }
+
+    public OnScrollChangedCallback getOnScrollChangedCallback()
+    {
+        return mOnScrollChangedCallback;
+    }
+
+    public void setOnScrollChangedCallback(final OnScrollChangedCallback onScrollChangedCallback)
+    {
+        mOnScrollChangedCallback = onScrollChangedCallback;
+    }
+
+    /**
+     * Impliment in the activity/fragment/view that you want to listen to the webview
+     */
+    public static interface OnScrollChangedCallback
+    {
+        public void onScroll(int l, int t, int oldl, int oldt);
+    }
+}
